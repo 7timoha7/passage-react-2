401b5fe438158d2d598b76acaf28c4df7a2a46a0
Artem Markelov <artem77timoha77@gmail.com>
2024-02-06 14:07:02 +0600

    passage# - Доработал категории сделал , чтоб закрытие одной вкладки при раскрытии другой . Именно на одном уровне вложености .

--- frontend/src/features/MenuCategories/Categories.tsx
+++ frontend/src/features/MenuCategories/Categories.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import { ExpandMore, ChevronRight } from '@mui/icons-material'; // Импорт иконок
+import { ExpandMore, ChevronRight } from '@mui/icons-material';
 import './Categories.css';
 import { useNavigate } from 'react-router-dom';
 import { useAppSelector } from '../../app/hooks';
 import { selectFetchAllCategoriesLoading } from './menuCategoriesSlice';
-import Spinner from '../../components/UI/Spinner/Spinner'; // Импорт стилей
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 interface Category {
   _id: string;
@@ -34,20 +34,32 @@ const CategoryWrapper = styled.div`
 const CategoryItem = styled.div<{ $level: number }>`
   border-bottom: 1px solid #ccc;
   cursor: pointer;
-  transition: background-color 0.3s ease;
+  transition: background-color 0.4s ease;
   display: flex;
   align-items: center;
-  // Отступ от левого края в зависимости от уровня вложенности
-  padding: 10px 10px 10px ${(props) => props.$level * 20}px;
+  padding: 5px 5px 5px ${(props) => props.$level * 10}px;
   color: #ffffff;
 
   &:hover {
-    background-color: rgba(225, 167, 167, 0.38);
+    background: rgba(145, 145, 145, 0.58);
   }
 `;
 
-const SubCategoryList = styled.div`
+const SpecialCategoryItem = styled(CategoryItem)`
+  background: rgba(204, 115, 115, 0.29);
+
+  &:hover {
+    background: rgba(145, 145, 145, 0.58);
+  }
+`;
+
+const SubCategoryList = styled.div<{ $isOpen: boolean; $opacity: number }>`
   padding-left: 0;
+  max-height: ${({ $isOpen }) => ($isOpen ? '1000px' : '0')};
+  overflow: hidden;
+  transition: max-height 0.5s ease, transform 0.5s ease, opacity 0.5s ease;
+  opacity: ${({ $opacity }) => $opacity};
+  transform: translateY(${({ $isOpen }) => ($isOpen ? '0' : '-100%')});
 `;
 
 const IconWrapper = styled.div`
@@ -57,12 +69,9 @@ const IconWrapper = styled.div`
 const Categories: React.FC<Props> = ({ categories, close }) => {
   const [categoryTree, setCategoryTree] = useState<HierarchicalCategory[]>([]);
   const [openState, setOpenState] = useState<OpenState>({});
+  const [subCategoriesOpacity, setSubCategoriesOpacity] = useState<number>(1);
   const navigate = useNavigate();
   const loading = useAppSelector(selectFetchAllCategoriesLoading);
-  const navigateAndClose = (item: string) => {
-    navigate('products/' + item);
-    close();
-  };
 
   useEffect(() => {
     const buildCategoryTree = (categories: Category[], parentID?: string): HierarchicalCategory[] => {
@@ -79,11 +88,28 @@ const Categories: React.FC<Props> = ({ categories, close }) => {
     setCategoryTree(tree);
   }, [categories]);
 
-  const handleCategoryClick = (categoryId: string) => {
-    setOpenState((prevOpenState) => ({
-      ...prevOpenState,
-      [categoryId]: !prevOpenState[categoryId],
-    }));
+  const handleCategoryClick = (categoryId: string, ownerID?: string) => {
+    setOpenState((prevOpenState) => {
+      const updatedOpenState: OpenState = { ...prevOpenState, [categoryId]: !prevOpenState[categoryId] };
+
+      if (ownerID) {
+        Object.keys(prevOpenState).forEach((id) => {
+          if (prevOpenState[id] && id !== categoryId && categories.find((cat) => cat.ID === id)?.ownerID === ownerID) {
+            updatedOpenState[id] = false;
+          }
+        });
+      }
+
+      return updatedOpenState;
+    });
+
+    setSubCategoriesOpacity(0);
+    setTimeout(() => setSubCategoriesOpacity(1), 50);
+  };
+
+  const navigateAndClose = (item: string) => {
+    navigate('products/' + item);
+    close();
   };
 
   const renderCategories = (categories: HierarchicalCategory[] | undefined, $level = 0) => {
@@ -100,22 +126,20 @@ const Categories: React.FC<Props> = ({ categories, close }) => {
             {categories.map((category) => (
               <div key={category.ID}>
                 {category.subCategories && category.subCategories.length > 0 ? (
-                  <CategoryItem $level={$level} onClick={() => handleCategoryClick(category.ID)}>
+                  <CategoryItem $level={$level} onClick={() => handleCategoryClick(category.ID, category.ownerID)}>
                     <IconWrapper>{openState[category.ID] ? <ExpandMore /> : <ChevronRight />}</IconWrapper>
                     {category.name}
                   </CategoryItem>
                 ) : (
-                  <CategoryItem
-                    style={{ background: 'rgba(194,145,145,0.71)' }}
-                    $level={$level + 1}
-                    onClick={() => navigateAndClose(category.ID)}
-                  >
+                  <SpecialCategoryItem $level={$level + 1} onClick={() => navigateAndClose(category.ID)}>
                     <IconWrapper />
                     {category.name}
-                  </CategoryItem>
+                  </SpecialCategoryItem>
                 )}
                 {openState[category.ID] && category.subCategories && (
-                  <SubCategoryList>{renderCategories(category.subCategories, $level + 1)}</SubCategoryList>
+                  <SubCategoryList $isOpen={openState[category.ID]} $opacity={subCategoriesOpacity}>
+                    {renderCategories(category.subCategories, $level + 1)}
+                  </SubCategoryList>
                 )}
               </div>
             ))}
