08108afea0bbe8216f4e66246352e3efa9d696d5
Artem Markelov <artem77timoha77@gmail.com>
2024-02-29 16:53:17 +0600

    passage# - neww

--- tests/steps.d.ts
+++ tests/steps.d.ts
@@ -1,10 +1,10 @@
 /// <reference types='codeceptjs' />
-type steps_file = typeof import('./steps_file.js');
+
 
 declare namespace CodeceptJS {
   interface SupportObject { I: I, current: any }
   interface Methods extends Puppeteer {}
-  interface I extends ReturnType<steps_file> {}
+  interface I extends WithTranslation<Methods> {}
   namespace Translation {
     interface Actions {}
   }
