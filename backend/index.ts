e7048702398a9dca051c593aab355efe4d3cdd5c
Artem Markelov <artem77timoha77@gmail.com>
2024-03-05 17:37:34 +0600

    passage# - new38

--- backend/index.ts
+++ backend/index.ts
@@ -21,14 +21,14 @@ const staticFilesPath = path.join(__dirname, 'public');
 app.use(cors());
 app.use(express.static(staticFilesPath));
 app.use(express.json());
-// app.use(
-//   cors({
-//     credentials: true,
-//     methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
-//     allowedHeaders: ['Content-Type', 'Authorization'],
-//     origin: ['http://passage.kg', 'https://passage.kg'],
-//   }),
-// );
+app.use(
+  cors({
+    credentials: true,
+    methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    origin: ['http://passage.kg/', 'https://passage.kg/'],
+  }),
+);
 app.use('/users', usersRouter);
 app.use('/categories', categoryRouter);
 app.use('/products', productRouter);
