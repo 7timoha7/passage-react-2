d7e6dd76a520f8e63b25de44058c46636b952ff6
Artem Markelov <artem77timoha77@gmail.com>
2024-03-06 15:34:54 +0600

    passage# - new52

--- backend/fixtures.ts
+++ backend/fixtures.ts
@@ -2,8 +2,7 @@ import mongoose from 'mongoose';
 import config from './config';
 import crypto from 'crypto';
 import User from './models/User';
-import * as http from 'http';
-import * as https from 'https';
+import axios from 'axios';
 
 const run = async () => {
   mongoose.set('strictQuery', false);
@@ -30,76 +29,45 @@ const run = async () => {
     isVerified: true,
   });
 
-  interface AuthData {
-    clientID: string;
-  }
-
-  interface GeneralData {
-    method: string;
-    deviceID: string;
-  }
-
-  interface RequestData {
-    auth: AuthData;
-    general: GeneralData;
-  }
-
-  // Здесь вам нужно заменить any на реальный тип данных, которые возвращает ваш запрос
-  interface ResponseData {
-    // Замените any на реальные типы данных
-  }
-
-  const fetchData = async (method: string): Promise<ResponseData> => {
-    const apiUrl = 'https://fresh-test.1c-cloud.kg/a/edoc/hs/ext_api/execute';
+  const fetchData = async (method: string) => {
+    const apiUrl = 'https://212.112.98.42/a/edoc/hs/ext_api/execute';
     const username = 'AUTH_TOKEN';
     const password = 'jU5gujas';
 
-    const requestOptions: http.RequestOptions = {
-      hostname: 'fresh-test.1c-cloud.kg',
-      port: 443,
-      path: '/a/edoc/hs/ext_api/execute',
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Basic ${Buffer.from(`${username}:${password}`, 'utf-8').toString('base64')}`,
-        configName: 'AUTHORIZATION',
-        configVersion: 'Basic Auth',
-      },
-      timeout: 600000, // Увеличьте значение таймаута по необходимости
-    };
+    try {
+      const response = await axios.post(
+        apiUrl,
+        {
+          auth: {
+            clientID: '422ba5da-2560-11ee-8135-005056b73475',
+          },
+          general: {
+            method,
+            deviceID: '00000001-0001-0001-0001-000000015941',
+          },
+        },
+        {
+          headers: {
+            Authorization: `Basic ${Buffer.from(`${username}:${password}`, 'utf-8').toString('base64')}`,
+            configName: 'AUTHORIZATION',
+            configVersion: 'Basic Auth',
+          },
+        },
+      );
 
-    const postData: RequestData = {
-      auth: {
-        clientID: '422ba5da-2560-11ee-8135-005056b73475',
-      },
-      general: {
-        method,
-        deviceID: '00000001-0001-0001-0001-000000015941',
-      },
-    };
-
-    return new Promise((resolve, reject) => {
-      const protocol = apiUrl.startsWith('https') ? https : http;
-
-      const req = protocol.request(requestOptions, (res) => {
-        let data = '';
-
-        res.on('data', (chunk) => {
-          data += chunk;
-        });
-
-        res.on('end', () => {
-          resolve(JSON.parse(data));
-        });
-      });
-
-      req.on('error', (error) => {
-        reject(error);
-      });
-
-      req.write(JSON.stringify(postData));
-      req.end();
-    });
+      return response.data;
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        if (error.code === 'ETIMEDOUT') {
+          console.error('Превышен таймаут при выполнении запроса:', error);
+        } else {
+          console.error('Ошибка при выполнении запроса:', error.message, error.response?.data);
+        }
+      } else {
+        console.error('Не удалось выполнить запрос:', error);
+      }
+      throw error;
+    }
   };
 
   const responseProducts = await fetchData('goods-get');
