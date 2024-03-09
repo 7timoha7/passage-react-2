347acee725b5724b14bb3462f411ccb13af00554
Artem Markelov <artem77timoha77@gmail.com>
2024-03-09 11:46:14 +0600

    passage# - 2

--- backend/routers/users.ts
+++ backend/routers/users.ts
@@ -322,7 +322,7 @@ usersRouter.post('/getVerify', auth, async (req, res, next) => {
       secure: false,
       auth: {
         user: config.mail,
-        pass: 'jczr vpof kqyp qrtj',
+        pass: 'mljz rkug aduq fxqw',
       },
     });
     // const mailOptions = {
