51921aed0d8aa717be86b923130a6e3a34a1bddc
Artem Markelov <artem77timoha77@gmail.com>
2024-03-11 11:57:39 +0600

    passage# - 13

--- backend/fixtures.ts
+++ backend/fixtures.ts
@@ -4,31 +4,30 @@ import crypto from 'crypto';
 import User from './models/User';
 
 const run = async () => {
-    mongoose.set('strictQuery', false);
-    await mongoose.connect(config.db);
-    const db = mongoose.connection;
+  mongoose.set('strictQuery', false);
+  await mongoose.connect(config.db);
+  const db = mongoose.connection;
 
-    try {
-        await db.dropCollection('users');
-        await db.dropCollection('baskets');
-        await db.dropCollection('orders');
-        await db.dropCollection('chatidadmins');
-    } catch (e) {
-        console.log('Collections were not present, skipping drop...');
-    }
+  try {
+    await db.dropCollection('users');
+    await db.dropCollection('baskets');
+    await db.dropCollection('orders');
+    await db.dropCollection('chatidadmins');
+  } catch (e) {
+    console.log('Collections were not present, skipping drop...');
+  }
 
-    await User.create({
-        email: 'director@gmail.com',
-        firstName: 'Director',
-        lastName: 'Directorovich',
-        password: '123',
-        token: crypto.randomUUID(),
-        role: 'director',
-        phoneNumber: '0555 888888',
-        isVerified: true,
-    });
+  await User.create({
+    email: 'director@gmail.com',
+    firstName: 'Director',
+    lastName: 'Directorovich',
+    password: '123',
+    token: crypto.randomUUID(),
+    role: 'director',
+    phoneNumber: '0555 888888',
+    isVerified: true,
+  });
 
-
-    await db.close();
+  await db.close();
 };
 run().catch(console.error);
