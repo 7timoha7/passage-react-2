a212162c72bd311e598cfe19781518c04ffe6a9f
Artem Markelov <artem77timoha77@gmail.com>
2024-04-09 17:02:27 +0600

    +++++ новая база - delete.ts

---
+++ backend/delete.ts
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+import config from './config';
+import User from './models/User';
+
+const run = async () => {
+  mongoose.set('strictQuery', false);
+  await mongoose.connect(config.db);
+  const db = mongoose.connection;
+
+  try {
+    await db.dropDatabase();
+    console.log('delete all data base');
+  } catch (e) {
+    console.log('Collections were not present, skipping drop...');
+  }
+
+  await User.create({
+    email: 'director@gmail.com',
+    firstName: 'director',
+    lastName: 'director',
+    password: '123',
+    token: crypto.randomUUID(),
+    role: 'director',
+    phoneNumber: '0555 888888',
+    isVerified: true,
+  });
+
+  await db.close();
+};
+run().catch(console.error);
