b5d92d977fa9120676ccb86cb7ad83ae9a328e7c
user <artem77timoha77@gmail.com>
2024-04-23 11:13:25 +0600

    ##### new 1

--- frontend/src/styles.ts
+++ frontend/src/styles.ts
@@ -13,38 +13,23 @@ export const ToolBarTopText = {
   ':hover': { color: '#ddbe86' },
 };
 
-// export const testColor = {
-//   color: '#179ae3',
-// };
-
 export const ToolBarStyles = {
-  // background: 'linear-gradient(45deg, hsla(0, 0%, 100%, 1) 0%, hsla(210, 79%, 46%, 1) 100%)',
-  // background: '#ffffff',
   backgroundImage: `url(${img1})`,
   backgroundSize: 'repeat',
   top: 0,
   py: 2.5,
 };
 export const ToolBarMobileStyles = {
-  // background: 'linear-gradient(225deg, hsla(210, 100%, 79%, 1) 0%, hsla(210, 79%, 46%, 1) 100%)',
   backgroundImage: `url(${img1})`,
   backgroundSize: 'repeat',
   top: 0,
   py: 1.5,
 };
 
-export const ProductsNewsBorderStyles = '5px solid #179ae3';
-
 export const FooterStyle = {
   // background: 'linear-gradient(45deg, hsla(210, 100%, 88%, 1) 0%, hsla(210, 79%, 46%, 1) 100%)',
   backgroundImage: `url(${img1})`,
   backgroundSize: 'repeat',
-};
-
-// export const menuCategoriesColor = 'linear-gradient(225deg, hsla(210, 100%, 79%, 1) 0%, hsla(210, 79%, 46%, 1) 100%)';
-
-export const HeaderStyles = {
-  color: '#00337C',
 };
 
 export const styleModalCover = {
@@ -70,16 +55,6 @@ export const styleModalCoverNew = {
   width: '90%',
   maxHeight: '90vh', // Вы можете установить максимальную высоту в виде процента от высоты экрана
   overflowY: 'auto', // Это свойство добавит вертикальную прокрутку при необходимости
-};
-
-export const arrowStyleGallery = {
-  position: 'absolute',
-  top: '50%',
-  transform: 'translateY(-50%)',
-  color: 'white',
-  background: 'rgba(0, 0, 0, 0.2)',
-  borderRadius: '50%',
-  zIndex: 1,
 };
 
 export const someStyle = {
