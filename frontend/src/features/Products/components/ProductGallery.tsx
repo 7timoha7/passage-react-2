f968035002e568ae31f80a800b1c3655ae5cbcf6
Artem Markelov <artem77timoha77@gmail.com>
2024-05-17 14:20:19 +0600

    #Passage - Сделал у фулскиновой картинки кнопку закрыть и на фулкарте уменьшил размер контейнера .

--- frontend/src/features/Products/components/ProductGallery.tsx
+++ frontend/src/features/Products/components/ProductGallery.tsx
@@ -2,20 +2,20 @@ import React from 'react';
 import ImageGallery from 'react-image-gallery';
 import 'react-image-gallery/styles/css/image-gallery.css';
 import { apiURL } from '../../../constants';
+import './BtnClose.css';
 
 interface Props {
   images: string[];
 }
 
 const ProductGallery: React.FC<Props> = ({ images }) => {
-  // Преобразование строк в объекты ImageType
   const galleryImages = images.map((image) => ({
     original: apiURL + '/' + image,
     thumbnail: apiURL + '/' + image,
-    originalHeight: 800, // Замените на нужную высоту
-    originalWidth: 600, // Замените на нужную ширину
-    thumbnailMaxHeight: 150, // Замените на нужную высоту
-    thumbnailMaxWidth: 100, // Замените на нужную ширину
+    originalHeight: 800,
+    originalWidth: 600,
+    thumbnailMaxHeight: 150,
+    thumbnailMaxWidth: 100,
   }));
 
   const handleScreenChange = (isFullScreen: boolean) => {
@@ -28,21 +28,40 @@ const ProductGallery: React.FC<Props> = ({ images }) => {
 
   const handleKeyDown = (e: KeyboardEvent) => {
     if (e.key === 'Escape') {
-      galleryRef.current?.fullScreen();
+      galleryRef.current?.exitFullScreen();
     }
   };
 
   const galleryRef = React.useRef<ImageGallery>(null);
 
   return (
-    <div>
-      <ImageGallery
-        ref={galleryRef}
-        items={galleryImages}
-        onScreenChange={handleScreenChange}
-        onClick={() => galleryRef.current?.fullScreen()}
-        lazyLoad={true} // Включение ленивой загрузки
-      />
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <div style={{ maxWidth: '1200px' }}>
+        <ImageGallery
+          ref={galleryRef}
+          items={galleryImages}
+          onScreenChange={handleScreenChange}
+          onClick={() => galleryRef.current?.fullScreen()}
+          lazyLoad={true}
+          renderFullscreenButton={(onClick, isFullscreen) =>
+            isFullscreen ? (
+              <div
+                style={{
+                  position: 'absolute',
+                  top: '10px',
+                  right: '10px',
+                }}
+              >
+                <button className="button" onClick={() => galleryRef.current?.exitFullScreen()}>
+                  <span className="X"></span>
+                  <span className="Y"></span>
+                  <div className="close">Close</div>
+                </button>
+              </div>
+            ) : null
+          }
+        />
+      </div>
     </div>
   );
 };
